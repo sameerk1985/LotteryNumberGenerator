fceb5982b550b5a36787042f856455c2b125e4d5
sameerk1985 <sameer.kottana@outlook.com>
2020-06-22 11:16:24 +0100

    checkin

---
+++ LotteryNumberGenerator/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace LotteryNumberGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.lblErrorMsg = new System.Windows.Forms.Label();
+            this.lvLotteryNumbers = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(167, 42);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(117, 23);
+            this.btnGenerate.TabIndex = 0;
+            this.btnGenerate.Text = "Generate Numbers";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // lblErrorMsg
+            // 
+            this.lblErrorMsg.AutoSize = true;
+            this.lblErrorMsg.ForeColor = System.Drawing.Color.Red;
+            this.lblErrorMsg.Location = new System.Drawing.Point(132, 64);
+            this.lblErrorMsg.Name = "lblErrorMsg";
+            this.lblErrorMsg.Size = new System.Drawing.Size(0, 13);
+            this.lblErrorMsg.TabIndex = 7;
+            // 
+            // lvLotteryNumbers
+            // 
+            this.lvLotteryNumbers.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.lvLotteryNumbers.ForeColor = System.Drawing.SystemColors.Window;
+            this.lvLotteryNumbers.HideSelection = false;
+            this.lvLotteryNumbers.Location = new System.Drawing.Point(12, 117);
+            this.lvLotteryNumbers.Name = "lvLotteryNumbers";
+            this.lvLotteryNumbers.Size = new System.Drawing.Size(433, 40);
+            this.lvLotteryNumbers.TabIndex = 8;
+            this.lvLotteryNumbers.UseCompatibleStateImageBehavior = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.ClientSize = new System.Drawing.Size(463, 180);
+            this.Controls.Add(this.lvLotteryNumbers);
+            this.Controls.Add(this.lblErrorMsg);
+            this.Controls.Add(this.btnGenerate);
+            this.Name = "Form1";
+            this.Text = "Lottery Number Generator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Label lblErrorMsg;
+        private System.Windows.Forms.ListView lvLotteryNumbers;
+    }
+}
+
---
+++ LotteryNumberGenerator/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LotteryNumberGenerator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static Random random = new Random();
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            try {
+                lvLotteryNumbers.Clear();
+
+                List<int> lotteryNumbers = GenerateLotteryNumbers(1, 49, 6);
+
+                if(lotteryNumbers != null && lotteryNumbers.Count > 0)
+                {
+                    lblErrorMsg.Text = string.Empty;
+
+                    foreach(int number in lotteryNumbers)
+                    {
+                        ListViewItem listViewItem = new ListViewItem
+                        {
+                            Text = number.ToString()
+                        };
+
+                        switch (number)
+                        {
+                            case int n when (n >= 1 && n <= 9):
+                                listViewItem.ForeColor = Color.Gray;
+                                break;
+                            case int n when (n >= 10 && n <= 19):
+                                listViewItem.ForeColor = Color.Black;
+                                break;
+                            case int n when (n >= 20 && n <= 29):
+                                listViewItem.ForeColor = Color.Pink;
+                                break;
+                            case int n when (n >= 30 && n <= 39):
+                                listViewItem.ForeColor = Color.Green;
+                                break;
+                            case int n when (n >= 40 && n <= 49):
+                                listViewItem.ForeColor = Color.Yellow;
+                                break;
+                        }
+
+                        lvLotteryNumbers.Items.Add(listViewItem);
+                    }
+                }
+                else
+                {
+                    lblErrorMsg.Text = "Unable to generate numbers, please try again.";
+                }
+            }
+            catch(Exception ex)
+            {
+                lblErrorMsg.Text = ex.Message;
+            }
+        }
+
+        private List<int> GenerateLotteryNumbers(int minValue, int maxValue, int count)
+        {
+            try
+            {
+                if (maxValue <= minValue || count < 0 || (count > maxValue - minValue && maxValue - minValue > 0))
+                {
+                    throw new ArgumentOutOfRangeException("Range " + minValue + " to " + maxValue +
+                            " (" + (maxValue - minValue) + " values), or count " + count + " is illegal");
+                }
+
+                HashSet<int> randomNumbers = new HashSet<int>();
+
+                for(int topValue = maxValue - count; topValue < maxValue; topValue++)
+                {
+                    if(!randomNumbers.Add(random.Next(minValue, topValue + 1)))
+                    {
+                        randomNumbers.Add(topValue);
+                    }
+                }
+
+                List<int> result = randomNumbers.ToList();
+
+                for (int i = result.Count - 1; i > 0; i--)
+                {
+                    int k = random.Next(i + 1);
+                    int tmp = result[k];
+                    result[k] = result[i];
+                    result[i] = tmp;
+                }
+
+                return result;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+    }
+}
